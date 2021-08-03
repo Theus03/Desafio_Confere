ce852e714d44d719cbffccab75d0926c2333b0d3
Theus03 <matheusssleite@outlook.com>
2021-08-03 17:42:30 -0300

    Create Home, Femininos, Masculinos and Infantis pages.

---
+++ public/src/scripts/index.js
@@ -0,0 +1,27 @@
+// Determina quanto tempo mostra a imagem antes de trocar
+let time = 7000,
+    currentImageWeb = 0,
+    images = document.querySelectorAll(".sliders img")
+    max = images.length;
+
+    // Função que troca a imagem
+    function nextImage(){
+        images[currentImageWeb].classList.remove("selected")
+       
+        currentImageWeb++
+
+        if(currentImageWeb >= max)
+            currentImageWeb = 0
+
+        images[currentImageWeb].classList.add("selected")  
+    }
+
+    // Função para executar o slider
+    function startImage(){
+        setInterval(() => {
+            //Troca Imagem
+            nextImage();
+        }, time)
+    }
+
+window.addEventListener("load", startImage)
